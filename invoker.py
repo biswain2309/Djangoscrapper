29ae308ad2f6d2522e4cea03f3d27f685e43fe87
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2021-01-05 23:33:41 +0530

    Exception handling while using Celery

--- invoker.py
+++ invoker.py
@@ -1,15 +1,18 @@
 import time
-from tasks import add
+# from tasks import add
+from tasks import data_extractor
 from celery.result import AsyncResult
 
 #Producer part of code
-result = add.delay(1,2)
+# result = add.delay(1,2)
 
-while True:
-    _result2 = AsyncResult(result.task_id)
-    status = _result2.status
-    print(status)
-    if 'SUCCESS' in status:
-        print('result after 5 sec wait {_result2}'.format(_result2=_result2.get()))
-        break
-    time.sleep(5)
+data_extractor.delay()
+
+# while True:
+#     _result2 = AsyncResult(result.task_id)
+#     status = _result2.status
+#     print(status)
+#     if 'SUCCESS' in status:
+#         print('result after 5 sec wait {_result2}'.format(_result2=_result2.get()))
+#         break
+#     time.sleep(5)
--- tasks.py
+++ tasks.py
@@ -4,9 +4,31 @@ from celery import Celery
 app = Celery('tasks', backend = 'redis://localhost:6379/0', broker = 'redis://localhost:6379/0')
 
 ##Consumer part of code
-@app.task(name='tasks.add')
-def add(x, y):
-    total = x + y
-    print('{} + {} = {}'.format(x, y, total))
-    time.sleep(10)
-    return total
+# @app.task(name='tasks.add')
+# def add(x, y):
+#     total = x + y
+#     print('{} + {} = {}'.format(x, y, total))
+#     time.sleep(10)
+#     return total
+
+
+#Exception handling in Celery
+
+##Exponential backoff
+def backoff(attempts):
+    """
+    1,2,4,8,16,32,...
+    """
+    return 2 ** attempts
+
+
+@app.task(bind=True, max_retries=4)
+def data_extractor(self):
+    try:
+        for i in range(1,11):
+            print('Crawling HTML DOM!')
+            if i == 5:
+                raise ValueError('Crwaling Index Error')
+    except Exception as exc:
+        print('Lets retry after 5 seconds')
+        raise self.retry(exc=exc, countdown=backoff(self.request.retries))
